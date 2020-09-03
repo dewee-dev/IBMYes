bf63d7e0e914c418dbebc945a65a313c416dc1cd
dewee-dev <61641407+dewee-dev@users.noreply.github.com>
2020-09-03 20:45:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ EOF
 
 clone_repo(){
     echo "进行初始化。。。"
-    git clone https://github.com/CCChieh/IBMYes
+    git clone https://github.com/dewee-dev/IBMYes
     cd IBMYes
     git submodule update --init --recursive
     cd v2ray-cloudfoundry/v2ray
@@ -103,8 +103,8 @@ install(){
     VMESSCODE=$(base64 -w 0 << EOF
     {
       "v": "2",
-      "ps": "ibmyes",
-      "add": "ibmyes.us-south.cf.appdomain.cloud",
+      "ps": "CCKnow",
+      "add": "CCKnow.eu-gb.cf.appdomain.cloud",
       "port": "443",
       "id": "${UUID}",
       "aid": "4",
